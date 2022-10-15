c364bfaebfe4af3902f506d2b7c7d346bbc2a9e1
Malachi9311 <ntuthuko93dlamini@gmail.com>
2022-10-15 07:24:56 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	ch = 'a'
+	while (ch <= 'z' && ch != 'e' || ch != 'q')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
