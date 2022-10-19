cb10443bc463fe00e98a4020c0f11eb6421240b8
Malachi9311 <ntuthuko93dlamini@gmail.com>
2022-10-19 08:19:09 -0700

    task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	posiitive_or_negative(i);
+
+	return (0);
+}
