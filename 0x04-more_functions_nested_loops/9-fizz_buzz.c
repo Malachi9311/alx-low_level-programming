18b0986670dab9ecc5b241b2aa2ca9b62c2bade7
Malachi9311 <ntuthuko93dlamini@gmail.com>
2022-10-20 12:54:38 -0700

    task09

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - print fizz buzz
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("fizzbuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("buzz ");
+		}
+		else
+			printf("%d ", n);
+	}
+	return (0);
+}
