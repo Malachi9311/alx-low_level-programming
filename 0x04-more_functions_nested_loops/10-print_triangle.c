8c60359a7ac74d5ae6c7dea150473fca90839851
Malachi9311 <ntuthuko93dlamini@gmail.com>
2022-10-20 13:34:35 -0700

    task10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle -prints a triangle
+ * @n: size of the triangle
+ * Return: empty
+ */
+
+void print_triangle(int n)
+{
+	int i;
+	int j;
+	int k;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n - i; j++)
+				_putchar(' ');
+			for (k = 1; k <= i; k++)
+				_putchar('*');
+			if (i == n)
+				continue;
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
