978ab2c502498aa63ccc9aa7bc9a1ae011db053a
Malachi9311 <ntuthuko93dlamini@gmail.com>
2022-10-24 12:38:27 -0700

    task04

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints s im reverse
+ * @s: string to be printed in reverse
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
