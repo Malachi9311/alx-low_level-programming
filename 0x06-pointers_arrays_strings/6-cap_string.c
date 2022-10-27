1186706b7586a4c6d8d0d5e019fd8ee97499f2ea
Malachi9311 <ntuthuko93dlamini@gmail.com>
2022-10-27 07:13:18 -0700

    task06

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes words
+ * @a: string
+ * Return: pointer to string
+ */
+
+char *cap_string(char *a)
+{
+	int i;
+
+	i = 0;
+	while (a[i])
+	{
+		while (!(a[i] >= 'a' && a[i] <= 'z'))
+			i++;
+		if (a[i - 1] == ' ' ||
+		a[i - 1] == '\t' ||
+		a[i - 1] == '\n' ||
+		a[i - 1] == ',' ||
+		a[i - 1] == ';' ||
+		a[i - 1] == '.' ||
+		a[i - 1] == '!' ||
+		a[i - 1] == '?' ||
+		a[i - 1] == '"' ||
+		a[i - 1] == '(' ||
+		a[i - 1] == ')' ||
+		a[i - 1] == '{' ||
+		a[i - 1] == '}' ||
+		i == '0')
+			a[i] -= 32;
+		i++;
+	}
+	return (a);
+}
