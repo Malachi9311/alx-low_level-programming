9917836bae8d4869f03c93f1646a972bfc619b32
Malachi9311 <ntuthuko93dlamini@gmail.com>
2022-11-02 10:24:36 -0700

    tgask03

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - factorial tutorial
+ * @n: input
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
